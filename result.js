624825130718ef592d16354d74d8457ac6beaf40
iykuwk <7yashzingade@gmail.com>
2025-09-23 18:59:03 +0530

    Model and Reult page with integration

---
+++ result.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const bars = document.querySelectorAll(".fill");
+  bars.forEach(bar => {
+    const width = bar.style.width;
+    bar.style.width = "0";
+    setTimeout(() => {
+      bar.style.width = width;
+    }, 200);
+  });
+
+
+});
+
+/
+
